aaaba0a9725cb8dbc639865f53430cf70536da19
Azhar Sayyed <azhartsiddiqui@gmail.com>
2023-10-22 16:44:19 +0530

    email template creation

--- app.ts
+++ app.ts
@@ -5,6 +5,7 @@ export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./middleware/error";
+import userRouter from "./routes/user.route";
 
 // body Parser
 app.use(express.json({ limit: "50mb" }));
@@ -14,6 +15,9 @@ app.use(cookieParser());
 
 // cors => cross origin resource sharing
 app.use(cors({ origin: process.env.ORIGIN }));
+
+// routes
+app.use("/api/v1", userRouter);
 
 // testing API
 app.get("/test", (req: Request, resp: Response, next: NextFunction) => {
